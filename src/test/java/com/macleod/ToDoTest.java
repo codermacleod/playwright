9f4de3cd979a5163dde4eccf94a2355a13f67fd5
Daniel Macleod <codermacleod@gmail.com>
2023-01-03 10:42:46 +0000

    Buy Some Milk exercise completed

--- src/test/java/com/macleod/ToDoTest.java
+++ src/test/java/com/macleod/ToDoTest.java
@@ -1,26 +1,51 @@
 package com.macleod;
 
 import com.microsoft.playwright.*;
-import com.microsoft.playwright.options.AriaRole;
 import org.junit.jupiter.api.*;
 
 import java.nio.file.Paths;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
 public class ToDoTest {
     static Playwright playwright;
     static Browser browser;
     BrowserContext context;
     Page page;
-    Locator reactButton;
+    Locator addItem, checkedBox;
 
     @Test
-    void shouldPrintPageTitle() {
-        page.navigate("https://todomvc.com");
-        System.out.println("First page" + page.title());
-        reactButton = page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setExact(true).setName("React"));
-        reactButton.click();
-        page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("mvc.png")));
-        System.out.println("Next page" + page.title());
+    void shouldAddItemToListAndThenCheckBoxToMarkAsComplete() {
+        //Setup:
+        page.navigate("https://todomvc.com/examples/react/#/");
+
+        //Check page title:
+        assertThat(page).hasTitle("React • TodoMVC");
+
+        //Add item and assert it has been added to list:
+        addItem = page.getByPlaceholder("What needs to be done?");
+        addItem.click();
+        page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("beforeMilk.png")));
+        addItem.fill("Buy some milk");
+        addItem.press("Enter");
+
+        //Confirm item was added:
+        assertThat(page.getByText("1 item left")).equals(true);
+        page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("afterMilk.png")));
+        System.out.println("It appears to have been added to the list...");
+
+
+        System.out.println("I'll now tick the checkbox...");
+        //Grab and check checkbox, then confirm with screenshot:
+        checkedBox = page.locator("input.toggle");
+        checkedBox.check();
+        page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("afterChecked.png")));
+
+        //Confirm item was checked off:
+        System.out.println("Final test about to run...");
+        assertThat(page.getByText("0 items left")).equals(true);
+        System.out.println("Final test finished.:)");
+
     }
     @BeforeAll
     static void launchBrowser() {
